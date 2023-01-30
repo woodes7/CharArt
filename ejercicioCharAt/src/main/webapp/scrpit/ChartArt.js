aaead5a0d1936378f80045d606716af2bc0dc27c
woodes7 <115068112+woodes7@users.noreply.github.com>
2023-01-30 10:03:14 +0100

    hasdsaf

---
+++ ejercicioCharAt/src/main/webapp/scrpit/ChartArt.js
@@ -0,0 +1,12 @@
+let texto1 = "Primera linea.\n Segunda linea.\n";
+alert(texto1);
+document.write(texto1);
+function ejemploCharArt()
+{
+	  var cualquierCadena="Brave new world";
+
+	  document.write("El caracter en el indice 0 es '" + cualquierCadena.charAt(0) + "'\n")
+	  document.write("El caracter en el indice 1 es '" + cualquierCadena.charAt(1) + "'\n")
+  	  document.write("El caracter en el indice 2 es '" + cualquierCadena.charAt(2) + "'\n")
+      document.write("El caracter en el indice 3 es '" + cualquierCadena.charAt(3) + "'\n")
+}
